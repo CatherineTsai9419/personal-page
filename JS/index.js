9e20e840b1983c8944047e97c98fc07a5dbf7438
Catherine-Tsai <peggy00208@gmail.com>
2020-11-08 01:33:52 +0800

    2020/11/8

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#firstimg").fadeIn(1500);
+    $('#clickhere').fadeIn(3000);
+    $('#clickdot1').fadeIn(3000);
+});
+
+function firstclick(){
+    $("#firstimg").fadeOut(1500);
+    $("#secondimg").fadeIn(1500);
+}
